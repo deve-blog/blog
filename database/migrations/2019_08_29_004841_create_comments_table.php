75dfb3eaa614e1c9042c3d3ede47ebc1f37934ae
kyang <kyang@i9i8.com>
2019-08-29 09:18:13 +0800

    数据库配置

---
+++ database/migrations/2019_08_29_004841_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->nullable(false)->comment('评论者的姓名');
+            $table->string('email', 50)->nullable(false)->comment('评论者的邮箱');
+            $table->text('content')->nullable(false)->comment('评论内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
