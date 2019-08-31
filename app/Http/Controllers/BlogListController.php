a1d4e4e5b7be2ca52e04c629999b520b4f7808b0
kyang <kyang@i9i8.com>
2019-08-31 17:39:13 +0800

    去掉没用的use

--- app/Http/Controllers/BlogListController.php
+++ app/Http/Controllers/BlogListController.php
@@ -6,9 +6,7 @@ use App\Blog;
 use App\Category;
 use App\Comment;
 use App\Paginate;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 
 class BlogListController extends Controller
 {
